deb55d4284cd45174fe803967591108c7f7150fd
Jarmo Pätsi <c7paja00@students.oamk.fi>
2020-03-24 22:03:20 +0200

    ui-web kansio ja alustava nettisivun runko luotu.

---
+++ ui-web/ui-web/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Parkissa!</h1>
+      <p>Tähän valmistuu Parkissa-projektin nettisivu.</p>
+      <p>...Under Construction...</p>
+    </div>
+  );
+}
+
+export default App;
