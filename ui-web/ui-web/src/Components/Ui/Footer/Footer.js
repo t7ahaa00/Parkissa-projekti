0f664b32d7951b268384f346d0e16d2831fef126
Jarmo Pätsi <c7paja00@students.oamk.fi>
2020-03-30 22:42:32 +0300

    korjaus

--- ui-web/ui-web/src/Components/Ui/Footer/Footer.js
+++ ui-web/ui-web/src/Components/Ui/Footer/Footer.js
@@ -7,13 +7,9 @@ const Footer = (props) => {
     
 
     return(
-<<<<<<< HEAD
-        <div className={classes.Footer}>...Under Construction...</div>
-=======
         <div className={classes.Footer}>
             © mepasme
         </div>
->>>>>>> 5e370d1e61a38daf2eef69fc421235695afb76df
     );
 };
 
