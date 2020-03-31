867d32fef6e5b69b1b50b906f653c3279b4abb02
Jarmo Pätsi <c7paja00@students.oamk.fi>
2020-03-31 17:22:56 +0300

    MapComponentin viilausta

--- ui-web/ui-web/src/Components/Ui/Body/Body.js
+++ ui-web/ui-web/src/Components/Ui/Body/Body.js
@@ -6,26 +6,11 @@ class Body extends Component {
 	render() {
 		return(
 		<div className={classes.Body}>
-			<MapComponent 
-				// id="myMap"
-				// options={{
-				// 	center: { lat: 65.0595, lng: 25.4662 },
-				// 	zoom: 16,
-				// 	mapTypeControl: true,
-					
-				// }}
-				// onMapLoad={map => {
-				// 	var marker = new window.google.maps.Marker({
-				// 	position: { lat: 65.0594, lng: 25.4711 },
-				// 	map: map,
-				// 	title: 'Parkki!'
-          		// 	});
-        		// }}
+			<MapComponent 	
 			/>
 		</div>
 		);
 	}
-
 };
 
 export default Body;
