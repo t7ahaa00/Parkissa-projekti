6ecb083008d40d1348ac9716e01da6808af12298
Jarmo Pätsi <c7paja00@students.oamk.fi>
2020-04-05 18:15:00 +0300

    Lisätty toiminnalisuus useamman parkkialueen näyttämämisen suhteen. InfoWindow toimii myös kaikilla uusilla parkkipaikoilla.

--- ui-web/ui-web/src/App.js
+++ ui-web/ui-web/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 import './App.css';
-import Header from './Components/Ui/Header/Header';
+import Header from './Components/Header/Header';
 import Body from './Components/Ui/Body/Body';
-import Footer from './Components/Ui/Footer/Footer';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
